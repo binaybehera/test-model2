acc0747329c0a8e7d7c03382c6bd16eecc21a47c
binaybehera <87566892+binaybehera@users.noreply.github.com>
2021-10-05 12:21:39 +0530

    Add files via upload

--- savemodel.py
+++ savemodel.py
@@ -2,6 +2,7 @@ from model import predict, save_score_data, get_metrics, train_model
 import os
 import json
 import pickle
+import sys
 #from flask import jsonify
 
 #defining sample forecasts dictionary
@@ -13,5 +14,5 @@ models = [{ 'id': 1, 'model_type': 'time series model with fbprophet'}]
 
 # start the app!
 if __name__ == '__main__':
-    train_model('initial_data.csv', ['ds', 'y'])
+    train_model(sys.argv[1], ['ds', 'y'])
 
